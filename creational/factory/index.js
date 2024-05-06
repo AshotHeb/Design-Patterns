59fab7cdc587db0216ee8faf7a47378d53cf088e
AshotHeb <heboyanashot@gmail.com>
2024-05-06 14:16:03 +0400

    Design patterns

---
+++ creational/factory/index.js
@@ -0,0 +1,31 @@
+class Audi {
+  constructor(year) {
+    this.name = "Audi";
+    this.year = year;
+  }
+}
+
+class BMW {
+  constructor(year) {
+    this.name = "BMW";
+    this.year = year;
+  }
+}
+
+class CarFactory {
+  constructor(type, year) {
+    switch (type) {
+      case "Audi":
+        return new Audi(year);
+      case "BMW":
+        return new BMW(year);
+      default:
+        return null;
+    }
+  }
+}
+
+const audi = new CarFactory("Audi", 2020);
+console.log(audi);
+const bmw = new CarFactory("BMW", 2020);
+console.log(bmw);
