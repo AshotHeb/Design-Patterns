63e4d73c33a9498c50c50641db1c70706ba5a1cd
AshotHeb <heboyanashot@gmail.com>
2024-05-21 10:02:46 +0400

    Proxy design pattern

---
+++ structural/proxy/index.js
@@ -0,0 +1,32 @@
+class OriginalImage {
+  constructor(filename) {
+    this.filename = filename;
+    this.loadFromDisk();
+  }
+
+  display() {
+    console.log(`Displaying ${this.filename}`);
+  }
+
+  loadFromDisk() {
+    console.log(`Loading ${this.filename}`);
+  }
+}
+
+class ProxyImage {
+  constructor(filename) {
+    this.filename = filename;
+  }
+
+  display() {
+    if (!this.originalImage) {
+      this.originalImage = new OriginalImage(this.filename);
+    }
+    this.originalImage.display();
+  }
+}
+
+const proxyImage = new ProxyImage("photo1.jpg");
+proxyImage.display();
+proxyImage.display();
+proxyImage.display();
